c9ebcefd7b9deed430985384dc413298d8880263
Marcin Siwonia <mahinek12@gmail.com>
2023-04-03 20:20:16 +0200

    Add convertPLNToUSD tests

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,20 @@
+export const convertPLNToUSD = (PLN) => {
+  if ( PLN === undefined) {
+    return NaN;
+  } else if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  } else if (typeof PLN === 'string') {
+    return NaN;
+  } else if (PLN < 0) {
+    return '$0.00';
+  }
+
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
